62a7b180fa0a46fd259e882d7743e76b3b1f3c7d
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-07-25 11:19:16 -0700

    Added Redis Control Interface

--- Redis/ViewController.swift
+++ Redis/ViewController.swift
@@ -13,22 +13,44 @@ class ViewController: UIViewController {
     
     let redisServer = Redis()
     let manager = CMMotionManager()
+    let scale: Double = 0.01
+    var yaw = -0.5
+    var pitch: Double = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Create Redis Server
-        redisServer.server("127.0.0.1", onPort: 6379)
+        redisServer.server("10.21.161.18", onPort: 6379)
         redisServer.Command("Ping")
+        redisServer.Command("set scl_pos_ee_des \(yaw) 0.8 \(pitch)")
         
         // CoreMotion functions
         manager.deviceMotionUpdateInterval = 0.01
         
-        manager.startDeviceMotionUpdates(to: OperationQueue.main(), withHandler:{
+        manager.startDeviceMotionUpdates(to: OperationQueue.main(), withHandler: {
             deviceManager, error in
             
-            print(deviceManager?.attitude.pitch)
-            print(deviceManager?.attitude.yaw)
+            if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) <= 25.00 {
+                print("Tilt forward")
+                self.pitch += self.scale
+                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+            } else if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) >= 75.00 {
+                print("Tilt backward")
+                self.pitch -= self.scale
+                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+            }
+            
+            if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) <= -25.00 {
+                print("Tilt right")
+                self.yaw += self.scale
+                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+            } else if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) >= 25.00 {
+                print("Tilt left")
+                self.yaw -= self.scale
+                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+            }
+            
             
         })
     
