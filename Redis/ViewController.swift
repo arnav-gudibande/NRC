80bfca6fe6d2795084c1c3da54c54e13f8da792d
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-07-27 10:12:37 -0700

    Stable Release

--- Redis/ViewController.swift
+++ Redis/ViewController.swift
@@ -9,25 +9,17 @@
 import UIKit
 import CoreMotion
 
-<<<<<<< HEAD
-
 class ViewController: UIViewController, UITextFieldDelegate {
     
     let redisServer = Redis()
     let manager = CMMotionManager()
-    let scale: Double = 0.007
-=======
-class ViewController: UIViewController {
-    
-    let redisServer = Redis()
-    let manager = CMMotionManager()
-    let scale: Double = 0.02
->>>>>>> parent of b47b870... UI & Functionality Changes
+    let scale: Double = 0.01
     var yaw = -0.5
     var pitch: Double = 0
+    var i = 0
 
-<<<<<<< HEAD
     @IBOutlet weak var statusLabel: UILabel!
+    
     @IBOutlet weak var ipAddress: UITextField!
     
     @IBAction func resetRedis(_ sender: AnyObject) {
@@ -41,21 +33,16 @@ class ViewController: UIViewController {
         i+=1
         
         if i%2 != 0 {
-            
             if let userTypedIP = ipAddress.text {
                 redisServer.server(userTypedIP, onPort: 6379)
             }
             redisServer.server(ipAddress.placeholder!, onPort: 6379)
             redisServer.Command("Ping")
-            
-            if redisServer.value(forKey: "starting") != nil {
-                startControl()
-                statusLabel.text = "Connected"
-                statusLabel.textColor = UIColor.green()
-                sender.setTitle("Shutdown Server", for: UIControlState.normal)
-                sender.setTitleColor(UIColor.red(), for: UIControlState.normal)
-            }
-            
+            startControl()
+            statusLabel.text = "Connected"
+            statusLabel.textColor = UIColor.green()
+            sender.setTitle("Shutdown Server", for: UIControlState.normal)
+            sender.setTitleColor(UIColor.red(), for: UIControlState.normal)
         } else if i%2==0 {
             redisServer.Command("SHUTDOWN")
             sender.setTitle("Connect to Server", for: UIControlState.normal)
@@ -64,60 +51,51 @@ class ViewController: UIViewController {
             statusLabel.textColor = UIColor.red()
         }
         
-        
     }
     
     func startControl() {
         redisServer.Command("set scl_pos_ee_des "+"\"" + "\(yaw) 0.8 \(pitch)" + "\"")
         //CoreMotion functions
-        manager.deviceMotionUpdateInterval = 0.07
-=======
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        manager.deviceMotionUpdateInterval = 0.1
         
-        // Create Redis Server
-        redisServer.server("10.21.161.18", onPort: 6379)
-        redisServer.Command("Ping")
-        redisServer.Command("set scl_pos_ee_des \(yaw) 0.8 \(pitch)")
-        
-        // CoreMotion functions
-        manager.deviceMotionUpdateInterval = 0.01
->>>>>>> parent of b47b870... UI & Functionality Changes
-        
-        manager.startDeviceMotionUpdates(to: OperationQueue.main(), withHandler: {
+        manager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: {
             deviceManager, error in
             
             if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) <= 25.00 {
-                print("Tilt forward")
-                self.pitch += self.scale
-                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
-            } else if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) >= 75.00 {
-                print("Tilt backward")
+                // Tilt forward
                 self.pitch -= self.scale
-                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+                self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
+            } else if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) >= 75.00 {
+                // Tilt backward
+                self.pitch += self.scale
+                self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
             }
             
             if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) <= -25.00 {
-                print("Tilt right")
+                // Tilt right
                 self.yaw += self.scale
-                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+                self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
             } else if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) >= 25.00 {
-                print("Tilt left")
+                // Tilt Left
                 self.yaw -= self.scale
-                self.redisServer.Command("set scl_pos_ee_des \(self.yaw) 0.8 \(self.pitch)")
+                self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
             }
             
             
         })
+    }
     
-<<<<<<< HEAD
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ipAddress.placeholder = "10.21.160.61"
         self.ipAddress.delegate = self
         self.ipAddress.keyboardType = UIKeyboardType.numbersAndPunctuation
-=======
->>>>>>> parent of b47b870... UI & Functionality Changes
+        //ipAddress.returnKeyType =
     }
 
     override func didReceiveMemoryWarning() {
