5d3ee9d52373012587f542ff179a4e780f8416c6
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-07-27 19:19:04 -0700

    UI changes

--- Redis/ViewController.swift
+++ Redis/ViewController.swift
@@ -9,93 +9,89 @@
 import UIKit
 import CoreMotion
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
     let redisServer = Redis()
     let manager = CMMotionManager()
-    let scale: Double = 0.01
+    let scale: Double = 0.03
     var yaw = -0.5
     var pitch: Double = 0
     var i = 0
 
     @IBOutlet weak var statusLabel: UILabel!
-    
     @IBOutlet weak var ipAddress: UITextField!
     
     @IBAction func resetRedis(_ sender: AnyObject) {
+        
         yaw = -0.5
         pitch = 0
         redisServer.Command("set scl_pos_ee_des "+"\"" + "\(yaw) 0.8 \(pitch)" + "\"")
+        
     }
     
     @IBAction func connectToServer(_ sender: UIButton) {
-        // Create Redis Server
-        i+=1
-        
-        if i%2 != 0 {
-            if let userTypedIP = ipAddress.text {
-                redisServer.server(userTypedIP, onPort: 6379)
-            }
-            redisServer.server(ipAddress.placeholder!, onPort: 6379)
-            redisServer.Command("Ping")
-            startControl()
-            statusLabel.text = "Connected"
-            statusLabel.textColor = UIColor.green()
-            sender.setTitle("Shutdown Server", for: UIControlState.normal)
-            sender.setTitleColor(UIColor.red(), for: UIControlState.normal)
-        } else if i%2==0 {
-            redisServer.Command("SHUTDOWN")
-            sender.setTitle("Connect to Server", for: UIControlState.normal)
-            sender.setTitleColor(UIColor.green(), for: UIControlState.normal)
-            statusLabel.text = "Not Connected"
-            statusLabel.textColor = UIColor.red()
+            
+        if let userTypedIP = ipAddress.text {
+            redisServer.server(userTypedIP, onPort: 6379)
         }
+        
+        redisServer.server(ipAddress.placeholder!, onPort: 6379)
+        redisServer.Command("Ping")
+        
+        startControl()
+        
+        statusLabel.text = "Connected"
+        statusLabel.textColor = UIColor.green()
         
     }
     
     func startControl() {
+        
         redisServer.Command("set scl_pos_ee_des "+"\"" + "\(yaw) 0.8 \(pitch)" + "\"")
+        
         //CoreMotion functions
         manager.deviceMotionUpdateInterval = 0.1
         
-        manager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: {
-            deviceManager, error in
+        manager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { deviceManager, error in
             
             if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) <= 25.00 {
+                
                 // Tilt forward
                 self.pitch -= self.scale
                 self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
+                
             } else if ((deviceManager?.attitude.pitch)! * 180.0/M_PI) >= 75.00 {
+                
                 // Tilt backward
                 self.pitch += self.scale
                 self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
+                
             }
             
             if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) <= -25.00 {
+                
                 // Tilt right
                 self.yaw += self.scale
                 self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
+                
             } else if ((deviceManager?.attitude.yaw)! * 180.0/M_PI) >= 25.00 {
+                
                 // Tilt Left
                 self.yaw -= self.scale
                 self.redisServer.Command("set scl_pos_ee_des "+"\"" + "\(self.yaw) 0.8 \(self.pitch)" + "\"")
+                
             }
             
             
         })
-    }
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
+        
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.ipAddress.placeholder = "10.21.160.61"
-        self.ipAddress.delegate = self
-        self.ipAddress.keyboardType = UIKeyboardType.numbersAndPunctuation
-        //ipAddress.returnKeyType =
+        
     }
 
     override func didReceiveMemoryWarning() {
